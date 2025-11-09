dcaadf931fcd06e39de2969b8fa2eb5bd727d0fb
Valentina Dyuryagina <valentina95dyuryagina@yandex.ru>
2025-11-09 16:45:36 +0500

    update email.py

--- email.py
+++ email.py
@@ -2,33 +2,174 @@ email1 = {
     "subject": "Quarterly Report",
     "from": "Alice.Cooper@Company. ",
     "to": " bob_smith@Gmail.com ",
-    "body": "Hello Bob,\n\tHere is the quarterly report.\n\tPlease review and let me know your feedback.\n\nBest,\nAlice"
+    "body": "Hello Bob,\n\tHere is the quarterly report.\n\tPlease review and let me know your feedback.\n\nBest,\nAlice",
 }
 
 email2 = {
     "subject": " Weekd plans ",
     "from": "katya_yan@yandex. ",
     "to": "frid@mail. ",
-    "body": "\tHey!\nLet's go hiking this weekd.\nBring snacks!\n"
+    "body": "\tHey!\nLet's go hiking this weekd.\nBring snacks!\n",
 }
 
 email3 = {
     "subject": "Reminder: Meeting",
     "from": "ceo@corporation.com ",
     "to": " team_lead@outlook.com ",
-    "body": " "
+    "body": " ",
 }
 
 email4 = {
     "subject": " ",
     "from": " alex@business.net ",
     "to": " hr@company. ",
-    "body": "Hi HR,\nPlease find attached my updated CV.\nThanks!"
+    "body": "Hi HR,\nPlease find attached my updated CV.\nThanks!",
 }
 
 email5 = {
     "subject": "Project collaboration",
     "from": " partner@organization.org ",
     "to": "lead_dev@icloud.com ",
-    "body": "Hello,\nWe are interested in a partnership.\tPlease reply soon.\nRegards,\nTeam"
+    "body": "Hello,\nWe are interested in a partnership.\tPlease reply soon.\nRegards,\nTeam",
 }
+
+
+# 1. Создайте словарь email, содержащий следующие поля:
+# "subject" (тема письма), "from" (адрес отправителя), "to" (адрес получателя), "body" (текст письма).
+
+email = {
+    "subject": "Hello, my friend!",
+    "from": "your_friend@gmail.com",
+    "to": "my_email@ya.ru",
+    "body": "Hi! I would like to invite you to join me for a trip to Baikal. I hope you can come!",
+}
+
+
+# 2. Добавьте дату отправки: создайте переменную send_date как текущую дату в формате YYYY-MM-DD и
+# запишите её в email["date"].https://www.w3schools.com/python/python_datetime.asp
+
+# сделала import datetime
+send_date = datetime.datetime.now()
+
+email["date"] = "send_date"
+
+
+# 3. Нормализуйте e-mail адреса отправителя и получателя: приведите к нижнему регистру и уберите пробелы по краям.
+# Запишите обратно в email["from"] и email["to"].
+
+email1["from"] = email1["from"].lower().strip()
+
+email1["to"] = email1["to"].lower().strip()
+
+
+# 4.Извлеките логин и домен отправителя в две переменные login и domain.
+
+login = email["from"].split("@")[0]
+
+domain = email["from"].split("@")[1]
+
+
+# 5. Создайте сокращённую версию текста: возьмите первые 10 символов email["body"] и добавьте многоточие "...".
+# Сохраните в новый ключ словаря: email["short_body"].
+
+email["short_body"] = (email["body"][0:10]) + "..."
+
+
+# 6. Списки доменов: создайте список личных доменов['gmail.com','list.ru', 'yahoo.com','outlook.com','hotmail.com',
+# 'icloud.com','yandex.ru','mail.ru','list.ru','bk.ru','inbox.ru'] и список корпоративных доменов ['company.ru',
+# 'corporation.com','university.edu','organization.org','company.ru', 'business.net'].
+# с учетом того что там должны быть только уникальные значение
+
+personal_domains = {
+    "gmail.com",
+    "list.ru",
+    "yahoo.com",
+    "outlook.com",
+    "hotmail.com",
+    "icloud.com",
+    "yandex.ru",
+    "mail.ru",
+    "list.ru",
+    "bk.ru",
+    "inbox.ru",
+}
+
+corporate_domains = {
+    "company.ru",
+    "corporation.com",
+    "university.edu",
+    "organization.org",
+    "company.ru",
+    "business.net",
+}
+
+
+# 7. Проверьте что в списке личных и корпоративных доменов нет пересечений:
+# ни один домен не должен входить в оба списка одновременно.
+
+personal_domains & corporate_domains
+
+
+# 8. Проверьте «корпоративность» отправителя: создайте булеву переменную is_corporate,
+# равную результату проверки вхождения домена отправителя в список корпоративных доменов.
+
+is_corporate = domain in corporate_domains
+
+
+# 9.Соберите «чистый» текст сообщения без табов и переводов строк: замените "\t" и "\n" на пробел.
+# Сохраните в email["clean_body"].
+
+email5["clean_body"] = email5["body"].replace("\t", " ").replace("\n", " ")
+
+
+# 10. Сформируйте текст отправленного письма многострочной f-строкой и сохраните в email["sent_text"]:
+# Кому: {получатель}, от {отправитель} Тема: {тема письма}, дата {дата} {чистый текст сообщения}
+
+email5[
+    "sent_text"
+] = f"""Кому: {email5["to"]}, 
+от {email5["from"]} 
+Тема: {email5["subject"]}, 
+дата {email5["date"]} 
+{email5["clean_body"]}"""
+
+
+# 11. Рассчитайте количество страниц печати для email["sent_text"], если на 1 страницу помещается 500 символов.
+# Сохраните результат в переменную pages. Значение должно быть округленно в большую сторону.
+
+pages = (len(email5["sent_text"]) + 499) // 500
+
+
+# 12. Проверьте пустоту темы и тела письма: создайте переменные is_subject_empty, is_body_empty
+# в котором будет хранится что тема письма содержит данные
+
+is_subject_empty = not email5["subject"].strip()
+
+is_body_empty = not email5["body"].strip()
+
+
+# 13. Создайте «маску» e-mail отправителя: первые 2 символа логина + "***@" + домен.Запишите в email["masked_from"].
+
+email["masked_from"] = login[:2] + "***@" + domain
+
+
+# 14. Удалите из списка личных доменов значения "list.ru" и "bk.ru"
+
+personal_domains.remove("list.ru")
+
+personal_domains.remove("bk.ru")
+
+
+# Результаты
+print(email, email1, email5)
+
+print(
+    login,
+    domain,
+    personal_domains,
+    corporate_domains,
+    is_corporate,
+    pages,
+    is_subject_empty,
+    is_body_empty,
+)
